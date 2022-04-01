21bb2ce49ab1e47dfd6db63cd1bf67f162ae336b
Raquel <101020903+rcappotto@users.noreply.github.com>
2022-04-01 14:06:28 -0400

    Update caesar.js

--- src/caesar.js
+++ src/caesar.js
@@ -5,9 +5,37 @@
 
 const caesarModule = (function () {
   // you can add any code you want within this function scope
-
-  function caesar(input, shift, encode = true) {
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  
+ function caesar(input, shift, encode = true) {
     // your solution code here
+    if(shift === 0 || shift < -25 || shift > 25|| shift === undefined) {
+      return false;
+    }
+    if(encode === false){
+      shift = -shift;
+    }
+    const output = [];
+    input = input.toLowerCase();
+    for(let i = 0; i < input.length; i++){
+      const char = input[i];
+      let code = alphabet.indexOf(char);
+      if(code === -1){
+        output.push(char);
+      }
+      else{
+        code += shift;
+        if(code > 25){
+          code -= 26;
+        }
+        else if(code < 0){
+          code += 26
+        }
+        const newChar = alphabet[code];
+        output.push(newChar);
+      }
+    }
+    return output.join("");
   }
 
   return {
@@ -16,3 +44,4 @@ const caesarModule = (function () {
 })();
 
 module.exports = { caesar: caesarModule.caesar };
+
